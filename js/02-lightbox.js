283353cc2bd769725993e4660e673ce4f1162a90
Bronislav Dereniuk <d.bronuk.m@gmail.com>
2022-01-08 18:44:07 +0200

    Done hw 7

---
+++ js/02-lightbox.js
@@ -0,0 +1,35 @@
+import { galleryItems } from './gallery-items.js';
+
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryCard = document.querySelector('.gallery');
+galleryCard.insertAdjacentHTML('beforeend', createGalleryItemsMarkup(galleryItems))
+
+function createGalleryItemsMarkup(element) {
+    return element.map(({ preview, original, description }) => {
+        return `
+<a class="gallery__item" href="${original}">
+    <img class="gallery__image"
+    src="${preview}"
+    alt="${description}"
+</a >
+`;
+    }).join("");
+} 
+
+const galleryItemsContainer = document.querySelector(".gallery");
+galleryItemsContainer.addEventListener("click", onGalleryItemsContainerClick);
+
+function onGalleryItemsContainerClick(event) { 
+    event.preventDefault();
+
+    if (event.target.getAttribute("class") !== "gallery__image") {
+        return;
+    }
+
+    const urlImage = event.target.dataset.source;
+
+new SimpleLightbox('.gallery a', {captionDelay:250, captionsData: 'alt'});
+};
